022e0cd944af20b5bae0df27daed70907bfe816d
IYGR <ilchenko1egor2@gmail.com>
2022-08-09 00:34:14 +0300

    Starter logic and repo UPD

--- src/main/java/ua/edu/sumdu/volonteerProject/serviceImpl/UserLocationServiceImpl.java
+++ src/main/java/ua/edu/sumdu/volonteerProject/serviceImpl/UserLocationServiceImpl.java
@@ -1,6 +1,8 @@
 package ua.edu.sumdu.volonteerProject.serviceImpl;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.logging.Log;
 import org.apache.logging.log4j.Logger;
 //import org.modelmapper.ModelMapper;
@@ -24,6 +26,7 @@ import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class UserLocationServiceImpl implements UserLocationService {
     private static int CORES = 4;
     private static final double SOME_REGULAR_CONSTANT_KOEF = 10;
@@ -35,24 +38,42 @@ public class UserLocationServiceImpl implements UserLocationService {
 
     private final CitiesRepo citiesRepo;
 
-    private List<CoordinatesAndK> computeInnerKoefs(UserLocation currentUser, List<UserLocation> allUsers, int from, int to){
-        return null;
+    private double computeInnerKoefs(UserLocation currentUser, List<UserLocation> allUsers,double cityArea, int amount,  int from, int to){
+        double ret_value = 0;
+        CoordinatesAndK coordinatesAndK = new CoordinatesAndK(currentUser.getLocationCoordinates(), 0);
+        final double SQUARE_KOEF = SOME_REGULAR_CONSTANT_KOEF * (double) amount / cityArea;
+        for (int i = from; i < to; i++) {
+            LocationCoordinates innerLC = allUsers.get(i).getLocationCoordinates();
+            LocationCoordinates outerLC = coordinatesAndK.locationCoordinates;
+            coordinatesAndK.k += exp(SQUARE_KOEF * -sqrt(
+                    pow((innerLC.getLatitude()) - outerLC.getLatitude(), 2)
+                            +
+                            pow((innerLC.getLongitude()) - outerLC.getLongitude(), 2)
+            ));
+
+        }
+        return coordinatesAndK.k;
     }
 
     private List<CoordinatesAndK> computeKoefsMT(List<UserLocation> userLocations,City city, int amount, int from, int to) {
+
         List<CoordinatesAndK> coordinatesAndKS = new ArrayList<>();
         for (int i= from; i<to; i++) {
+            List<CompletableFuture<Double>> completableFutures = new ArrayList<>();
             CoordinatesAndK coordinatesAndK = new CoordinatesAndK(userLocations.get(i).getLocationCoordinates(), 0);
-            final double SQUARE_KOEF = SOME_REGULAR_CONSTANT_KOEF * (double) amount / city.getArea();
-
-            for (UserLocation t : userLocations) {
-                LocationCoordinates innerLC = t.getLocationCoordinates();
-                LocationCoordinates outerLC = coordinatesAndK.locationCoordinates;
-                coordinatesAndK.k += exp(SQUARE_KOEF * -sqrt(
-                        pow((innerLC.getLatitude()) - outerLC.getLatitude(), 2)
-                                +
-                                pow((innerLC.getLongitude()) - outerLC.getLongitude(), 2)
-                ));
+            int finalI = i;
+            completableFutures.add(  CompletableFuture.supplyAsync(()-> this.computeInnerKoefs(userLocations.get(finalI), userLocations,city.getArea(),amount, 0,userLocations.size()/4)).exceptionally((e) -> {log.error("exception in  computeKoefsMT",e); return null;}));
+            completableFutures.add(CompletableFuture.supplyAsync(()-> this.computeInnerKoefs(userLocations.get(finalI), userLocations,city.getArea(),amount, userLocations.size()/4,userLocations.size()/2)).exceptionally((e) -> {log.error("exception in  computeKoefsMT",e); return null;}));
+            completableFutures.add(CompletableFuture.supplyAsync(()-> this.computeInnerKoefs(userLocations.get(finalI), userLocations,city.getArea(),amount, userLocations.size()/2,userLocations.size()*3/4)).exceptionally((e) -> {log.error("exception in  computeKoefsMT",e); return null;}));
+            completableFutures.add(CompletableFuture.supplyAsync(()-> this.computeInnerKoefs(userLocations.get(finalI), userLocations,city.getArea(),amount, userLocations.size()*3/4,userLocations.size())).exceptionally((e) -> {log.error("exception in  computeKoefsMT",e); return null;}));
+            for(CompletableFuture<Double> k : completableFutures){
+                try {
+                    coordinatesAndK.k+=k.get();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
             }
             coordinatesAndKS.add(coordinatesAndK);
         }
@@ -64,10 +85,13 @@ public class UserLocationServiceImpl implements UserLocationService {
         /*for(int i = 1 ; i<= CORES && userLocations.size()>CORES; i++){
             int finalI = i;
         }
-        */completableFutures.add(  CompletableFuture.supplyAsync(()-> this.computeKoefsMT(userLocations,city,amount, 0,userLocations.size()/4)));
-        completableFutures.add(CompletableFuture.supplyAsync(()-> this.computeKoefsMT(userLocations,city,amount,userLocations.size()/4,userLocations.size()/2)));
-        completableFutures.add(CompletableFuture.supplyAsync(()-> this.computeKoefsMT(userLocations,city,amount,userLocations.size()/2,(userLocations.size()*3)/4)));
-        completableFutures.add(CompletableFuture.supplyAsync(()-> this.computeKoefsMT(userLocations,city,amount,((userLocations.size()*3)/4),userLocations.size())));
+        */
+
+
+            completableFutures.add(CompletableFuture.supplyAsync(() -> this.computeKoefsMT(userLocations, city, amount, 0, userLocations.size() / 4)));
+            completableFutures.add(CompletableFuture.supplyAsync(() -> this.computeKoefsMT(userLocations, city, amount, userLocations.size() / 4, userLocations.size() / 2)));
+            completableFutures.add(CompletableFuture.supplyAsync(() -> this.computeKoefsMT(userLocations, city, amount, userLocations.size() / 2, (userLocations.size() * 3) / 4)));
+            completableFutures.add(CompletableFuture.supplyAsync(() -> this.computeKoefsMT(userLocations, city, amount, ((userLocations.size() * 3) / 4), userLocations.size())));
         List<CoordinatesAndK> list = new ArrayList<>();
         for (CompletableFuture t:
              completableFutures) {
