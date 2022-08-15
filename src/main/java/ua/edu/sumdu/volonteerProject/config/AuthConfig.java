18cfce1420a78e0733728b8025d51a68b3857662
Ilchenko Yehor <johndoe@example.com>
2022-08-15 14:33:04 +0300

    security upd

--- src/main/java/ua/edu/sumdu/volonteerProject/config/AuthConfig.java
+++ src/main/java/ua/edu/sumdu/volonteerProject/config/AuthConfig.java
@@ -4,6 +4,7 @@ package ua.edu.sumdu.volonteerProject.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,12 +17,6 @@ import ua.edu.sumdu.volonteerProject.security.CustomUserDetailsService;
 @Configuration
 @EnableWebSecurity
 public class AuthConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public UserDetailsService userDetailsService(@Autowired JwtUserDetailsRepository jwtUserDetailsRepository){
-        return new CustomUserDetailsService(jwtUserDetailsRepository);
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/ua/edu/sumdu/volonteerProject/security/JwtUserDetails.java
+++ src/main/java/ua/edu/sumdu/volonteerProject/security/JwtUserDetails.java
@@ -1,20 +1,22 @@
 package ua.edu.sumdu.volonteerProject.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtUserDetails implements UserDetails {
 
-    @ManyToMany(mappedBy = "userDetails")
+    @ManyToMany(mappedBy = "userDetails", fetch = FetchType.EAGER)
     private List<Authority> authorityList;
     private String password;
     @Id
